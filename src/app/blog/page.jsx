ac296a9d369426e33951e8a860d28a4af06135e0
ousman-ali <oasgerbalhl@gmail.com>
2025-12-11 11:43:26 +0300

    blogs and products with conditional rendering of featuredproducts passed to components

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,56 @@
 "use client";
 
+import { getBatchData } from "@/api/service";
 import BlogSection1 from "@/components/blog/BlogSection1";
 import FeaturedBlogPosts from "@/components/blog/FeaturedBlogPosts";
 import Breadcrumb from "@/components/breadcrumb/BreadCrumb";
 import CTACard from "@/components/cta/CtaSection";
+import Loading from "@/components/loading/Loading";
+import { useEffect, useState } from "react";
 
 export default function AboutPage() {
+  const [products, setProducts] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [featuredBlogs, setFeaturedBlogs] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const features = [
+      { name: "ecommerce_product", amount: 5 },
+      { name: "blog_post", amount: 100000 },
+    ];
+
+    async function fetchData() {
+      try {
+        const data = await getBatchData(features);
+        setProducts(data.ecommerce_product || []);
+        setBlogs(data.blog_post || []);
+        const featured = (data.blog_post?.data || []).filter(
+          (blog) => blog.is_featured === true
+        );
+
+        setFeaturedBlogs(featured);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false); // Global loading done!
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  console.log("blogs", blogs.data);
+
+  if (loading) {
+    return (
+      <div className="">
+        <Loading />
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black">
       {/* Breadcrumb Component */}
@@ -14,8 +59,8 @@ export default function AboutPage() {
         pageName="Our Blog Posts"
         currentPage="South Flower Posts"
       />
-      <FeaturedBlogPosts />
-      <BlogSection1 />
+      {featuredBlogs.length > 0 && <FeaturedBlogPosts blogs={featuredBlogs} />}
+      <BlogSection1 blogs={blogs.data} products={products.data} />
       <CTACard />
     </div>
   );
